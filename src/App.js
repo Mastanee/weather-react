551d237a8266316a8c36a6eea4fdc69927ee0180
Mastanee <mastane.kachloo@gmail.com>
2024-09-08 20:06:32 +0200

    changes css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import axios from "axios";
+
+import "./styles.css";
+
+export default function App() {
+  let [city, setCity] = useState();
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState(null);
+
+  function showWeather(response) {
+    setLoaded(true);
+
+    setWeather({
+      temperature: Math.round(response.data.main.temp - 270),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function showCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="input"
+        type="search"
+        placeholder="Enter a city..."
+        onChange={showCity}
+      />
+      <input className="button" type="submit" value="Search" />
+    </form>
   );
-}
+  if (loaded) {
+    return (
+      <div className="App">
+        {form}
+        <ul>
+          <li>Temperature :{weather.temperature} °C</li>
+          <li>Description :{weather.description}</li>
+          <li>Humidity :{weather.humidity} %</li>
+          <li>Wind :{weather.wind} km/h</li>
 
-export default App;
+          <img src={weather.icon} alt="weather icon" />
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
